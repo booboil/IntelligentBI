54ade0c19b1cb1304424174d1b8442673754dac0
张新荣 <2914424377@qq.com>
2024-01-02 13:43:12 +0800

    修改名称

---
+++ src/main/java/com/booboil/intelligentbi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.booboil.intelligentbi.exception;
+
+import com.booboil.intelligentbi.common.BaseResponse;
+import com.booboil.intelligentbi.common.ErrorCode;
+import com.booboil.intelligentbi.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author <a href="https://github.com/booboil">程序员booboil</a>
+ * 
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
---
+++ src/main/java/com/booboil/intelligentbi/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.booboil.intelligentbi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.booboil.intelligentbi.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author <a href="https://github.com/booboil">程序员booboil</a>
+ * 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
